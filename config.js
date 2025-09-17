8e7b84440f3af83f9779cba6e95138f55dcf1c0b
ZiviUser <jamie.schmidlin@schule-bremgarten.ch>
2025-09-17 16:05:41 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+<!-- config.js (neu) -->
+// Trage hier deine Firebase Config ein
+const firebaseConfig = {
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+databaseURL: "https://YOUR_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
+projectId: "YOUR_PROJECT",
+storageBucket: "YOUR_PROJECT.appspot.com",
+messagingSenderId: "YOUR_SENDER_ID",
+appId: "YOUR_APP_ID"
+};
